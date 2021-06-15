9744b6cc65afe670c681bf4321f4353d02b51f4c
Neilsmith Christian <chrisnei@sheridancollege.ca>
2021-06-15 16:00:09 +0200

    Initial Commit

---
+++ src/main/java/sheridan/chrisnei/assingment2/PetDataApplication.java
@@ -0,0 +1,13 @@
+package sheridan.chrisnei.assingment2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetDataApplication.class, args);
+    }
+
+}
