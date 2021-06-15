30f6005d2ab8c85349b1b52b1394ccff1c2fdfd3
Neilsmith Christian <chrisnei@sheridancollege.ca>
2021-06-15 16:20:34 +0200

    Added a PetKind enum

---
+++ src/main/java/sheridan/chrisnei/assingment2/domain/PetKind.java
@@ -0,0 +1,38 @@
+package sheridan.chrisnei.assingment2.domain;
+
+/**
+ * An enum representing a pet kind
+ */
+public enum PetKind {
+
+    /**
+     * A constant representing a dog
+     */
+    DOG {
+        @Override
+        public String toString() {
+            return "Dog";
+        }
+    },
+
+    /**
+     * A constant representing a cat
+     */
+    CAT {
+        @Override
+        public String toString() {
+            return "Cat";
+        }
+    },
+
+    /**
+     * A constant representing a rabbit
+     */
+    RABBIT {
+        @Override
+        public String toString() {
+            return "Rabbit";
+        }
+    }
+
+}
