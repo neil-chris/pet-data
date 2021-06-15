f7ffa4d42fd57f1163726ba416d1120147114e79
Neilsmith Christian <chrisnei@sheridancollege.ca>
2021-06-15 16:15:45 +0200

    Added comments to PetGender enum

--- src/main/java/sheridan/chrisnei/assingment2/domain/PetGender.java
+++ src/main/java/sheridan/chrisnei/assingment2/domain/PetGender.java
@@ -1,6 +1,13 @@
 package sheridan.chrisnei.assingment2.domain;
 
+/**
+ * An enum representing the gender of a pet
+ */
 public enum PetGender {
+
+    /**
+     * A constant representing a female gender
+     */
     FEMALE {
         @Override
         public String toString() {
@@ -8,6 +15,9 @@ public enum PetGender {
         }
     },
 
+    /**
+     * A constant representing a male gender
+     */
     MALE {
         @Override
         public String toString() {
