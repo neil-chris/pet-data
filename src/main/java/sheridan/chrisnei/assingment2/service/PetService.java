dc1924515a1f47dcbb1adba5e2397bfbc30bfd55
Neilsmith Christian <chrisnei@sheridancollege.ca>
2021-06-16 15:48:17 +0200

    Renamed PetDataService class to PetService and added annotated class as a service

--- src/main/java/sheridan/chrisnei/assingment2/service/PetService.java
+++ src/main/java/sheridan/chrisnei/assingment2/service/PetService.java
@@ -1,16 +1,18 @@
 package sheridan.chrisnei.assingment2.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import sheridan.chrisnei.assingment2.model.Pet;
 import sheridan.chrisnei.assingment2.repository.PetDataRepositoryJpa;
 import java.util.*;
 
-public class PetDataService {
+@Service
+public class PetService {
 
     @Autowired
     private PetDataRepositoryJpa repositoryJpa;
 
-    public PetDataService(PetDataRepositoryJpa repositoryJpa) {
+    public PetService(PetDataRepositoryJpa repositoryJpa) {
         this.repositoryJpa = repositoryJpa;
     }
 
