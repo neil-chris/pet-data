486c936a217a9ea19968a13d441ef2013fde7c51
Neilsmith Christian <chrisnei@sheridancollege.ca>
2021-06-16 14:05:57 +0200

    Removed unnecessary methods from PetDataService class

--- src/main/java/sheridan/chrisnei/assingment2/service/PetDataService.java
+++ src/main/java/sheridan/chrisnei/assingment2/service/PetDataService.java
@@ -1,13 +1,15 @@
 package sheridan.chrisnei.assingment2.service;
 
-import sheridan.chrisnei.assingment2.model.Gender;
-import sheridan.chrisnei.assingment2.model.Kind;
+import org.springframework.beans.factory.annotation.Autowired;
 import sheridan.chrisnei.assingment2.model.Pet;
 import sheridan.chrisnei.assingment2.repository.PetDataRepositoryJpa;
 import java.util.*;
 
 public class PetDataService {
+
+    @Autowired
     private PetDataRepositoryJpa repositoryJpa;
+
     public PetDataService(PetDataRepositoryJpa repositoryJpa) {
         this.repositoryJpa = repositoryJpa;
     }
@@ -16,27 +18,16 @@ public class PetDataService {
         return repositoryJpa.findById(id);
     }
 
-    public List<Pet> getPetsByKind(Kind kind) {
-        return repositoryJpa.findByKind(kind);
-    }
-
-    public List<Pet> getPetsByGender(Gender gender) {
-        return repositoryJpa.findByGender(gender);
-    }
-
     public List<Pet> getAllPets() {
        return repositoryJpa.findAll();
     }
 
     public void addPet(Pet pet){
-        repositoryJpa.add(pet);
+        repositoryJpa.save(pet);
     }
 
-    public void deletePet(int id) {
-        repositoryJpa.delete(id);
+    public void deletePet(Pet pet) {
+        repositoryJpa.removeById(pet.getId());
     }
 
-    public void deleteAllPets() {
-        repositoryJpa.deleteAll();
-    }
 }
