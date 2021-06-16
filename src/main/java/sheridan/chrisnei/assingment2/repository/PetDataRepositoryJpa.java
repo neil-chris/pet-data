81caf786e0b2cbf8dcfb21f841fc7a8fb6d5406a
Neilsmith Christian <chrisnei@sheridancollege.ca>
2021-06-16 13:38:05 +0200

    Removed unnecessary methods from PetDataRepositoryJpa

--- src/main/java/sheridan/chrisnei/assingment2/repository/PetDataRepositoryJpa.java
+++ src/main/java/sheridan/chrisnei/assingment2/repository/PetDataRepositoryJpa.java
@@ -2,17 +2,10 @@ package sheridan.chrisnei.assingment2.repository;
 
 import sheridan.chrisnei.assingment2.model.*;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-
 import java.util.*;
 
 public interface PetDataRepositoryJpa extends JpaRepository<Pet, Integer> {
     Pet findById(int id);
-    Pet findByName(String name);
-    List<Pet> findByKind(Kind kind);
-    List<Pet> findByGender(Gender gender);
     List<Pet> findAll();
-    void add(Pet pet);
-    void delete(int id);
-    void deleteAll();
+    void removeById(int id);
 }
