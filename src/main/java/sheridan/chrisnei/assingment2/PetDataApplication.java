8d5b5e972b16ac8c1b13fa354c69df8a77c7c5d5
Neilsmith Christian <chrisnei@sheridancollege.ca>
2021-06-17 06:42:54 +0200

    Modified PetDataApplication to add logging

--- src/main/java/sheridan/chrisnei/assingment2/PetDataApplication.java
+++ src/main/java/sheridan/chrisnei/assingment2/PetDataApplication.java
@@ -1,12 +1,15 @@
 package sheridan.chrisnei.assingment2;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class PetDataApplication {
 
     public static void main(String[] args) {
+        log.info("Invoked PetDataApplication.main()");
         SpringApplication.run(PetDataApplication.class, args);
     }
 
