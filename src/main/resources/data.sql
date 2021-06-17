d8032bedebad27ad91bce24aea899304fc55161d
Neilsmith Christian <chrisnei@sheridancollege.ca>
2021-06-17 09:51:06 +0200

    Added data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO pet (name, kind, gender, vaccinated) VALUES ('Boogie', 'DOG', 'MALE', true);
+INSERT INTO  pet (name, kind, gender, vaccinated) VALUES ('Purity', 'CAT', 'FEMALE', false);
+INSERT INTO pet (name, kind, gender, vaccinated) VALUES ('Bunny', 'RABBIT', 'MALE', true);
