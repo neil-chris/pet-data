245556c82b253a2aadc69976e2f0b9992cba216c
Neilsmith Christian <chrisnei@sheridancollege.ca>
2021-06-17 11:04:52 +0200

    Added styles.css

--- src/main/java/sheridan/chrisnei/assingment2/controller/PetController.java
+++ src/main/java/sheridan/chrisnei/assingment2/controller/PetController.java
@@ -1,20 +1,21 @@
 package sheridan.chrisnei.assingment2.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import sheridan.chrisnei.assingment2.model.Gender;
-import sheridan.chrisnei.assingment2.model.Kind;
 import sheridan.chrisnei.assingment2.model.Pet;
 import sheridan.chrisnei.assingment2.service.PetService;
-
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 import java.util.List;
 
 @Controller
 @RequestMapping("/pet")
+@Slf4j
 public class PetController {
 
     @Autowired
@@ -22,46 +23,52 @@ public class PetController {
 
     @GetMapping("/all")
     public String showAllPets(Model model) {
-        addPets();
+        log.info("Invoked PetController.showAllPets() method");
         List<Pet> pets = petService.getAllPets();
         model.addAttribute("pets", pets);
         return "all-pets";
     }
 
-    @GetMapping("/delete/{id}")
-    public String deletePet(@PathVariable("id") int id, Model model) {
-        Pet pet = petService.getPet(id);
-//                .orElseThrow(() -> new IllegalArgumentException("Invalid pet Id:" + id));
-        petService.deletePet(pet);
+    @GetMapping("/create")
+    public String showAddPetForm(Model model) {
+        log.info("Invoked PetController.showAddPetForm() method");
+        Pet pet = new Pet();
+        pet.setGender(Gender.FEMALE);
+        model.addAttribute("pet", pet);
+        return "create-pet";
+    }
+
+    @PostMapping("/create")
+    public String addPet( @Valid Pet pet, BindingResult result, Model model) {
+        log.info("Invoked PetController.addPet() method");
+        petService.addPet(pet);
         return "redirect:/pet/all";
     }
 
-    @GetMapping("/edit/{id}")
-    public String showUpdateForm(@PathVariable("id") int id, Model model) {
+    @Transactional
+    @GetMapping("/delete/{id}")
+    public String deletePet(@PathVariable("id") int id, Model model) {
+        log.info("Invoked PetController.deletePet() method");
+        petService.deletePet(id);
+        return "redirect:/pet/all";
+    }
+
+    @GetMapping("/update/{id}")
+    public String showUpdatePetForm(@PathVariable("id") int id, Model model) {
+        log.info("Invoked PetController.showUpdateForm() method");
         Pet pet = petService.getPet(id);
         model.addAttribute("pet", pet);
         return "update-pet";
     }
 
     @PostMapping("/update/{id}")
-    public String updatePet(@PathVariable("id") int id, @Valid Pet pet,
-                            BindingResult result, Model model) {
+    public String updatePet(@PathVariable("id") int id, @Valid Pet pet, BindingResult result, Model model) {
+        log.info("Invoked PetController.updatePet() method");
         if (result.hasErrors()) {
             pet.setId(id);
             return "update-pet";
         }
-
         petService.addPet(pet);
-        return "redirect:pet/all";
-    }
-
-    private void addPets() {
-        Pet pet1 = new Pet("Boogie", Kind.DOG, Gender.MALE, true);
-        Pet pet2 = new Pet("Purity", Kind.CAT, Gender.FEMALE, true);
-        Pet pet3 = new Pet("Bunny", Kind.RABBIT, Gender.MALE, true);
-
-        petService.addPet(pet1);
-        petService.addPet(pet2);
-        petService.addPet(pet3);
+        return "redirect:/pet/all";
     }
 }
