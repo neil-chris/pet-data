2cc6f51307697a40ddb5d0da5b4ca37ffc8d4b3b
Neilsmith Christian <chrisnei@sheridancollege.ca>
2021-06-17 06:42:24 +0200

    Annotated PetDataRepositoryJpa as a repository

--- src/main/java/sheridan/chrisnei/assingment2/repository/PetDataRepositoryJpa.java
+++ src/main/java/sheridan/chrisnei/assingment2/repository/PetDataRepositoryJpa.java
@@ -1,9 +1,11 @@
 package sheridan.chrisnei.assingment2.repository;
 
+import org.springframework.stereotype.Repository;
 import sheridan.chrisnei.assingment2.model.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.*;
 
+@Repository
 public interface PetDataRepositoryJpa extends JpaRepository<Pet, Integer> {
     Pet findById(int id);
     List<Pet> findAll();
